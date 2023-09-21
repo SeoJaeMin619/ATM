7143c60eb96265a54c3af7c64a29f9e324ce8fa2
SeoJaeMin619 <emiya1663@naver.com>
2023-09-21 23:04:04 +0900

    ui완성

---
+++ ATM/Assets/New Folder/Scripts/Withdraw.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Withdraw : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Withdraw");
+    }
+}
